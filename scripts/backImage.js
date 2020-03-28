428ac27eccd82e8806b2a4202081b025309ceaae
Mehul Pillai <mehulpillai30@gmail.com>
2020-03-28 11:58:09 -0400

    Adds random background image

---
+++ scripts/backImage.js
@@ -0,0 +1,42 @@
+const apiKey = "5e7f67a8f96f9f072a0b0a98"
+const getImages = () => {
+    const endpoint = "https://reddtwalls-8176.restdb.io/rest/images"
+    fetch(endpoint, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': apiKey
+            }
+        }).then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+
+            throw new Error('Failed to get image')
+        }, netError => console.log(netError.message))
+        .then(jsonResponse => setImage(jsonResponse))
+}
+
+const setImage = json => {
+    const bodyBack = document.body.style
+    let images = ''
+    console.log(json)
+    if (!json.length) {
+        console.log('No response for images')
+        return
+    }
+
+    for (let i = 0; i < json.length; i++) {
+        let currImage = json[i]
+        images += currImage['image'] + " "
+    }
+    let imageUrls = images.trim().split(" ")
+
+    console.log(imageUrls)
+    const randomIndex = Math.floor(Math.random() * imageUrls.length)
+    const chosenUrl = imageUrls[randomIndex]
+    console.log(chosenUrl)
+    bodyBack.backgroundImage = `url(${chosenUrl})`
+}
