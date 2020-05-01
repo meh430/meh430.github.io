40b35c133be9a573a479ad39f03bb2cec596cbc6
Mehul Pillai <mehulpillai30@gmail.com>
2020-05-01 09:46:36 -0400

    Moves data to remote

--- src/about/Skills.js
+++ src/about/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { skillsEndpoint, apiKey } from "../Consts";
 const header = {
     textAlign: "center",
     margin: "2%",
@@ -16,41 +17,64 @@ const fsize = {
     fontSize: "18px",
 };
 
-const skills = [
-    "Java",
-    "JavaScript",
-    "Kotlin",
-    "C++",
-    "Python",
-    "Dart",
-    "Android Jetpack",
-    "ReactJS",
-    "Bootstrap",
-    "Flutter",
-];
-
 export class Skills extends React.Component {
+    getSkills() {
+        fetch(skillsEndpoint, {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+                apikey: apiKey,
+            },
+        })
+            .then(
+                (response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+
+                    throw new Error("Failed to get skills");
+                },
+                (netError) => console.log(netError.message)
+            )
+            .then((jsonResponse) => setTimeout(() => this.parseSkills(jsonResponse), 1));
+    }
+
+    parseSkills(json) {
+        let cardList = [];
+        if (!json.length) {
+            console.log("No response for skills");
+            return;
+        }
+        for (let i = 0; i < json.length; i++) {
+            cardList.push(
+                <li key={`skill_${i}`}>
+                    <Card style={card}>
+                        <Card.Body style={fsize}>{json[i].skill}</Card.Body>
+                    </Card>
+                </li>
+            );
+        }
+
+        this.setState({sList: cardList})
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { sList: [<li key={`skill_${0}`}><h3>Please Wait..</h3></li>] };
+        this.parseSkills = this.parseSkills.bind(this)
+        this.getSkills = this.getSkills.bind(this)
+        setTimeout(() => this.getSkills(), 1)
+    }
+
     render() {
         return (
             <div align="center" style={{ margin: "3%" }}>
                 <hr style={{ width: "4%" }} />
                 <h1 style={header}>Languages and Frameworks</h1>
                 <br />
-                <ul style={{ width: "90%" }}>
-                    {(() => {
-                        let cardList = [];
-                        for (let i = 0; i < skills.length; i++) {
-                            cardList.push(
-                                <li key={`skill_${i}`}>
-                                    <Card style={card}>
-                                        <Card.Body style={fsize}>{skills[i]}</Card.Body>
-                                    </Card>
-                                </li>
-                            );
-                        }
-                        return cardList;
-                    })()}
-                </ul>
+                <ul style={{ width: "90%" }}>{this.state.sList}</ul>
             </div>
         );
     }
